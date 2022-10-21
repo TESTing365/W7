693b2384997ac8720675f234e7df5578f7e31a25
brjun <brj1001@163.com>
2022-10-21 18:12:40 +0800

    云参数设置本地化

---
+++ upgrade/1.0.7/up.php
@@ -0,0 +1,17 @@
+<?php
+namespace W7\U102;
+
+defined('IN_IA') or exit('Access Denied');
+class Up {
+    const DESCRIPTION = '兼容云参数设置问题';
+	public function up() {
+		if (!pdo_fieldexists('modules', 'cloudsetting')) {
+			pdo_query("ALTER TABLE " . tablename('modules') . " ADD `cloudsetting` VARCHAR(2000) NOT NULL DEFAULT '' COMMENT '云参数设置序列化值';");
+		}
+		return true;
+	}
+
+	public function down() {
+		return true;
+	}
+}
